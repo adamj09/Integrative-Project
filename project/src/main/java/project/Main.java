bbaeb6e7c157a90e561f46d48a17caef6ee689ba
Adam Johnston <adamjohnston144@gmail.com>
2026-02-21 14:35:45 -0500

    OpenGL Rendering Working

--- project/src/main/java/project/Main.java
+++ project/src/main/java/project/Main.java
@@ -2,23 +2,54 @@ package project;
 
 import javafx.application.*;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import com.huskerdev.openglfx.lwjgl.LWJGLExecutor;
+
 import com.huskerdev.openglfx.canvas.GLCanvas;
+
+import static org.lwjgl.opengl.GL11.*;
 
 public class Main extends Application {
 
     public void start(Stage stage) {
-        var canvas = new GLCanvas.Builder().setExecutor(LWJGLExecutor.LWJGL_MODULE).build();
+        System.setProperty("prism.vsync", "false");
 
-        Scene scene = new Scene(canvas, 1280, 720);
-        stage.setScene(scene);
+        StackPane rootPane = new StackPane();
+
+        rootPane.getChildren().add(createGLCanvas());
+
+        stage.setScene(new Scene(rootPane, 1280, 720));
+        stage.setTitle("Orbital Motion Simulator");
         stage.show();
     }
 
+    private GLCanvas createGLCanvas() {
+
+        GLCanvas.Builder glCanvasBuilder = new GLCanvas.Builder();
+        glCanvasBuilder.setFlipY(true);
+        glCanvasBuilder.setExecutor(LWJGLExecutor.LWJGL_MODULE);
+        glCanvasBuilder.setFps(60);
+        glCanvasBuilder.setMSAA(4);
+        glCanvasBuilder.setSwapBuffers(2);
+
+        var canvas = glCanvasBuilder.build();
+
+        canvas.addOnInitEvent(_ -> {
+
+        });
+
+        canvas.addOnRenderEvent(_ -> {
+            glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
+
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+        });
+
+        return canvas;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         launch(args);
     }
 }
