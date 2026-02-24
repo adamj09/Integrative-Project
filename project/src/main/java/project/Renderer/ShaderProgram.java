8abc14ec9d89d902d096c825b36a017e04281672
Adam Johnston <adamjohnston144@gmail.com>
2026-02-23 23:04:30 -0500

    shader compilation working

---
+++ project/src/main/java/project/Renderer/ShaderProgram.java
@@ -0,0 +1,100 @@
+package project.Renderer;
+
+import static org.lwjgl.opengl.GL46.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ShaderProgram {
+    private int ID;
+
+    ShaderProgram(String vertFilepath, String fragFilepath) throws IOException {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        var vertexShaderSourceFuture = executor.submit(new ShaderLoaderTask(vertFilepath));
+        var fragmentShaderSourceFuture = executor.submit(new ShaderLoaderTask(fragFilepath));
+
+        executor.shutdown();
+
+        String vertexShaderSource = "", fragmentShaderSource = "";
+
+        try {
+            vertexShaderSource = vertexShaderSourceFuture.get();
+            fragmentShaderSource = fragmentShaderSourceFuture.get();
+        } catch (InterruptedException | ExecutionException ex) {
+        }
+
+        // Create and compile shaders.
+        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, vertexShaderSource);
+        glCompileShader(vertexShader);
+
+        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, fragmentShaderSource);
+        glCompileShader(fragmentShader);
+
+        if (glGetShaderi(vertexShader, GL_COMPILE_STATUS) == 0) { // 0 means unsuccessful compilation; print shader info log.
+            System.err.println("Vertex shader compilation failed: " + glGetShaderInfoLog(vertexShader));
+            return;
+        }
+
+        if (glGetShaderi(fragmentShader, GL_COMPILE_STATUS) == 0) { // 0 means unsuccessful compilation; print shader info log.
+            System.err.println("Vertex shader compilation failed: " + glGetShaderInfoLog(fragmentShader));
+            return;
+        }
+
+        // Create the shader program using the two shaders.
+        ID = glCreateProgram();
+        glAttachShader(ID, vertexShader);
+        glAttachShader(ID, fragmentShader);
+        glLinkProgram(ID);
+ 
+        if (glGetProgrami(ID, GL_LINK_STATUS) == 0) {
+            System.err.println("Linking shader program failed: " + glGetProgramInfoLog(ID));
+            return;
+        }
+
+        // Delete shaders as they are no longer needed after program creation and linking.
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+    }
+
+    public void use() {
+        glUseProgram(ID);
+    }
+
+    private class ShaderLoaderTask implements Callable<String> {
+        String filepath;
+
+        public ShaderLoaderTask(String filepath) {
+            this.filepath = filepath;
+        }
+
+        @Override
+        public String call() throws IOException {
+            // Create new file for shader and check if exists.
+            File file = new File(filepath);
+            if (!file.exists()) {
+                System.err.println("Shader specified at " + filepath + " could not be found!");
+                throw new FileNotFoundException("Shader specified at " + filepath + " could not be found!");
+            }
+
+            // Read shader file.
+            String source;
+            try {
+                source = Files.readString(Path.of(file.getPath()));
+            } catch (IOException ex) {
+                throw ex;
+            }
+
+            return source;
+        }
+    }
+}
--- project/src/main/java/project/Renderer/SimRenderer.java
+++ project/src/main/java/project/Renderer/SimRenderer.java
@@ -2,24 +2,29 @@ package project.Renderer;
 
 import static org.lwjgl.opengl.GL46.*;
 
+import java.io.IOException;
+
 public class SimRenderer extends Renderer {
+    private ShaderProgram shaderProgram;
+
     public SimRenderer(double fps, int msaa, int swapBuffers) {
         super(fps, msaa, swapBuffers);
     }
 
     @Override
     public void init() {
-        //TODO: load shader code from file
-        //TODO: should probably create class to manage loading and compilation of shaders
-
-        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertexShader, "");
-        glCompileShader(vertexShader);
+        try {
+            shaderProgram = new ShaderProgram(
+            "Integrative-Project/project/shaders/main.vert", 
+            "Integrative-Project/project/shaders/main.frag"
+            );
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
     }
 
     @Override
     public void loop() {
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        shaderProgram.use();
     }
 }
