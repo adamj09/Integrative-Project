ab596b69231257711fb9cb32bc6f9da1c8ab2e43
Adam Johnston <adamjohnston144@gmail.com>
2026-02-24 00:51:50 -0500

    separate VBOs, vertex indices

--- project/src/main/java/project/Renderer/SimRenderer.java
+++ project/src/main/java/project/Renderer/SimRenderer.java
@@ -1,30 +1,16 @@
 package project.Renderer;
 
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_FLOAT;
-import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glDrawArrays;
-import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
-import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
-import static org.lwjgl.opengl.GL15.glBindBuffer;
-import static org.lwjgl.opengl.GL15.glBufferData;
-import static org.lwjgl.opengl.GL15.glGenBuffers;
-import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
-import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
-import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
-import static org.lwjgl.opengl.GL30.glBindVertexArray;
-import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 import static org.lwjgl.opengl.GL46.*;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 
 import org.lwjgl.BufferUtils;
 
 public class SimRenderer extends Renderer {
     private ShaderProgram shaderProgram;
-    private int VBO, VAO;
+    private int VAO, EBO;
+    private int[] VBO = new int[2];
 
     public SimRenderer(double fps, int msaa, int swapBuffers) {
         super(fps, msaa, swapBuffers);
@@ -37,31 +23,68 @@ public class SimRenderer extends Renderer {
                 "Integrative-Project/project/shaders/main.frag");
 
         float[] vertices = {
-                -0.5f, -0.5f, 0.0f,
+                0.5f,  0.5f, 0.0f,
                 0.5f, -0.5f, 0.0f,
-                0.0f, 0.5f, 0.0f
+                -0.5f, -0.5f, 0.0f,
+                -0.5f,  0.5f, 0.0f
         };
 
-        VAO = glGenVertexArrays();        
-        glBindVertexArray(VAO);
-        glEnableVertexAttribArray(0);
+        float[] colors = {
+            1.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 1.0f,
+            1.0f, 1.0f, 0.0f
+        };
+
+        int[] indices = {
+            0, 1, 3,
+            1, 2, 3
+        };
 
         FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
         verticesBuffer.put(vertices).flip();
 
-        VBO = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
+        FloatBuffer colorsBuffer = BufferUtils.createFloatBuffer(colors.length);
+        colorsBuffer.put(colors).flip();
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        IntBuffer indicesBuffer = BufferUtils.createIntBuffer(indices.length);
+        indicesBuffer.put(indices).flip();
+
+        VAO = glGenVertexArrays();        
+        VBO[0] = glGenBuffers();
+        VBO[1] = glGenBuffers();
+        EBO = glGenBuffers();
+
+        // Bind VAO FIRRSSTT then do everything else
+        glBindVertexArray(VAO);
+
+        // --- Vertex Attributes ---
+
+        // Note: order matters here: whatever buffer is binded must immediately be given
+        // an attribute pointer BEFORE binding the next buffer.
+        glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+        glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * Float.BYTES, 0);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+        glBufferData(GL_ARRAY_BUFFER, colorsBuffer, GL_STATIC_DRAW);
+        glVertexAttribPointer(1, 3, GL_FLOAT, false, 3 * Float.BYTES, 0);
+        glEnableVertexAttribArray(1);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL_STATIC_DRAW);
+
+        shaderProgram.use();
     }
 
     @Override
     public void loop() {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        shaderProgram.use();
-
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // Bind VAO and draw
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     }
 }
