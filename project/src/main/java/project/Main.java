f555064cc2cb70941b51e50523cda5bb332f72b8
Adam Johnston <adamjohnston144@gmail.com>
2026-03-14 23:07:07 -0400

    removed useless import

--- project/src/main/java/project/Main.java
+++ project/src/main/java/project/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import project.Renderer.ControlManager;
 import project.Renderer.Renderer;
 
 public class Main extends Application {
