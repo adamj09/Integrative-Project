9f5c8f155c8ba2c81c11b1548127c1ed24ca9958
AdamJ <adamjohnston144@gmail.com>
2026-03-23 13:51:54 -0400

    fix circle rendering

--- project/shaders/circle_frag.glsl
+++ project/shaders/circle_frag.glsl
@@ -2,7 +2,7 @@
 
 uniform vec2 resolution;
 
-vec3 create_circle(vec2 position, vec3 color, float size) {
+vec4 create_circle(vec2 position, vec4 color, float size) {
     float circle = sqrt(pow(position.x, 2.0) + pow(position.y + 2.0));
     circle = smoothstep(size, size + 0.003, 1.0 - circle);
 
@@ -12,12 +12,12 @@ vec3 create_circle(vec2 position, vec3 color, float size) {
 void main() {
     vec2 position = gl_FragCoord.xy / resolution;
 
-    float canvasColor = 0.0;
+    vec4 canvasColor = vec4(0.0);
     float circle = 0.0;
 
-    vec3 circle = create_circle(position - vec2(0.5, 0.5), vec3(1.0, 1.0, 1.0), 0.9);
+    vec3 circle = create_circle(position - vec2(0.5, 0.5), vec4(1.0, 1.0, 1.0, 1.0), 0.9);
 
-    canvas += circle;
+    canvasColor += circle;
 
-    gl_FragColor = vec4(canvas, 1.0);
+    gl_FragColor = vec4(canvasColor);
 }
