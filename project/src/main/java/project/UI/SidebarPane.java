7736d772d681df71c35e79915a7cb8be90656d83
AdamJ <adamjohnston144@gmail.com>
2026-04-21 15:12:58 -0400

    quick fix

--- project/src/main/java/project/UI/SidebarPane.java
+++ project/src/main/java/project/UI/SidebarPane.java
@@ -406,6 +406,7 @@ public class SidebarPane extends VBox {
 
         bottom.selectSatelliteForView(selectedBody, "");
 
+        pool.getRenderer().setFocusObject(selectedBody);
         pool.getCurrentWorld().removeSatellite(name);
     }
 
