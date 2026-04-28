e657db60ce6d0d09df4b9434a78222baacd70b46
Adam Johnston <adamjohnston144@gmail.com>
2026-04-27 23:42:34 -0400

    comments and cleanup

--- project/src/main/java/project/Presets/PresetConfiguration.java
+++ project/src/main/java/project/Presets/PresetConfiguration.java
@@ -5,38 +5,78 @@ import java.util.HashMap;
 import javafx.scene.paint.Color;
 
 /**
+ * Holds all the data required for a preset.
+ * 
  * @author Ryan Lau
  */
 public class PresetConfiguration {
+    /**
+     * The preset body's data.
+     */
     private final BodyPreset body;
+
+    /**
+     * A HashMap containing each satellite's data, keyed by satellite name.
+     */
     private final HashMap<String, SatellitePreset> satellites;
+
+    /**
+     * The preset for the BottomPane, used to save simulation settings.
+     */
     private final BottomPanePreset bottomPanePreset;
 
-    public PresetConfiguration(
-        BodyPreset body,
-        HashMap<String, SatellitePreset> satellites,
-        BottomPanePreset bottomPanePreset
-    ) {
+    /**
+     * Creates a new PresetConfiguration with all the required data.
+     * 
+     * @param body             the preset body's data.
+     * @param satellites       a HashMap containing each satellite's data, keyed by
+     *                         satellite name.
+     * @param bottomPanePreset the preset for the BottomPane, used to save
+     *                         simulation settings.
+     */
+    public PresetConfiguration(BodyPreset body, HashMap<String, SatellitePreset> satellites,
+            BottomPanePreset bottomPanePreset) {
         this.body = body;
         this.satellites = satellites;
         this.bottomPanePreset = bottomPanePreset;
     }
 
+    /**
+     * @return the body preset.
+     */
     public BodyPreset getBody() {
         return body;
     }
 
+    /**
+     * @return all satellite presets.
+     */
     public HashMap<String, SatellitePreset> getSatellites() {
         return satellites;
     }
 
+    /**
+     * @return the bottom pane preset.
+     */
     public BottomPanePreset getBottomPanePreset() {
         return bottomPanePreset;
     }
 
-    public record BodyPreset(String name, Color color, boolean preset, double mass, double radius) {}
+    /**
+     * Definitions of data held by a BodyPreset.
+     */
+    public record BodyPreset(String name, Color color, boolean preset, double mass, double radius) {
+    }
 
-    public record SatellitePreset(String name, Color color) {}
+    /**
+     * Definitions of data held by a SatellitePreset.
+     */
+    public record SatellitePreset(String name, Color color) {
+    }
 
-    public record BottomPanePreset(String specificTime, String timescale, boolean running) {}
+    /**
+     * Definitions of data held by a BottomPanePreset.
+     */
+    public record BottomPanePreset(String specificTime, String timescale, boolean running) {
+    }
 }
--- project/src/main/java/project/Presets/PresetFileService.java
+++ project/src/main/java/project/Presets/PresetFileService.java
@@ -9,18 +9,36 @@ import java.nio.file.Path;
 import com.google.gson.Gson;
 
 /**
+ * Class for saving and loading WorldConfigurations.
+ * 
  * @author Ryan Lau
  */
 public class PresetFileService {
-
+    /**
+     * JSON file to save data to, or load data from.
+     */
     private final Gson gson = GsonFactory.create();
 
+    /**
+     * Save data to JSON file.
+     * 
+     * @param filePath      path to save file to.
+     * @param configuration data to save.
+     * @throws IOException if the JSON file cannot be written to.
+     */
     public void save(Path filePath, WorldConfiguration configuration) throws IOException {
         try (Writer writer = Files.newBufferedWriter(filePath)) {
             gson.toJson(configuration, writer);
         }
     }
 
+    /**
+     * Load data from JSON file.
+     * 
+     * @param filePathpath to load file from.
+     * @return loaded data.
+     * @throws IOException if the JSON file cannot be read.
+     */
     public WorldConfiguration load(Path filePath) throws IOException {
         try (Reader reader = Files.newBufferedReader(filePath)) {
             WorldConfiguration config = gson.fromJson(reader, WorldConfiguration.class);
